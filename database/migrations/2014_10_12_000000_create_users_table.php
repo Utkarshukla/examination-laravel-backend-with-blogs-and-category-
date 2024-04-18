e147b9f7370530ae95eda528e49e6f0a6022a885
utkarshShukla <shukla@phantasm.co.in>
2024-04-18 15:54:02 +0530

    user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,24 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name'); //stduent //school
+            $table->string('email'); //stduent //school
+            $table->timestamp('email_verified_at')->nullable(); 
+            $table->string('phone')->nullable(); //stduent //school
+            $table->string('father')->nullable(); //stduent 
+            $table->string('mother')->nullable(); //stduent
+            $table->string('class')->nullable(); //stduent
+            $table->string('gender')->nullable(); //school
+            $table->date('dob')->nullable();//student 
+            $table->tinyInteger('role')->default(5); 
+            $table->string('city'); //stduent
+            $table->string('district'); //stduent
+            $table->string('state')->default('Andhra Pradesh');
+            $table->string('pincode'); //stduent
+            $table->string('school');//student //school
+            $table->string('school-code')->nullable(); 
             $table->string('google_id')->nullable();
-            $table->string('password')->nullable();
+            $table->string('password')->nullable(); //student //school
             $table->rememberToken();
             $table->timestamps();
         });
