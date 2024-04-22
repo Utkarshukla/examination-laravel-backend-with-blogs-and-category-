2f4ff8376c8fe1cdb042c2700b2ad3a9db917631
utkarshShukla <shukla@phantasm.co.in>
2024-04-22 18:55:28 +0530

    removed user_id and school_id in post requests

--- app/Http/Controllers/BulkParticipateController.php
+++ app/Http/Controllers/BulkParticipateController.php
@@ -26,7 +26,9 @@ class BulkParticipateController extends Controller
         $user_id =$user->id;
         $user_role=$user->role;
         if($user_role == 1 || $user_role ==2){
-            $validator = Validator::make($request->all(),[
+
+            foreach($request->all() as $requestdata){
+                $validator = Validator::make($requestdata,[
                 'name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'string', 'email', 'max:255'],
                 'aadhar_number'=> ['required','numeric','digits:12'],
@@ -38,11 +40,34 @@ class BulkParticipateController extends Controller
                 'city' => ['string'],
                 'district' => ['required', 'string'],
                 'pincode' => ['required', 'numeric'],
-                'school_unique_code' => ['required', 'numeric'],
-                'olympiad_id'=>['required'],
+                'school_unique_code' => ['required'],
+                'olympiad_id'=>['required','numeric'],
                 'subjects'=>['required']
             ]);
-            return response()->json(['error'=>'You are  allowed to register via csv'],201);
+            if($validator->fails()){
+                return response()->json(['error'=>$validator->errors()],422);
+            }
+
+            $name=$requestdata['name'];
+            $email=$requestdata['email'];
+            $aadhar=$requestdata['aadhar_number'];
+            $phone= $requestdata['phone'];
+            $father=$requestdata['father']??'NAN';
+            $mother=$requestdata['mother'] ??'NAN';
+            $class=$requestdata['class'];
+            $dob=$requestdata['dob'];
+            $city=$requestdata['city']?? 'Vijayawada';
+            $district = $requestdata['district'] ?? 'Krishna';
+            $pincode =$requestdata['pincode']?? 520007;
+            $school_code = $requestdata['school_unique_code'] ?? 'matrixmath';
+            $olympiad_id=$requestdata['olympiad_id'];
+            $subjects=$requestdata['subjects'];
+
+            return response()->json(['data'=>$subjects]);
+            }
+            
+            $data= 1;
+            return response()->json(['error'=>'You are  allowed to register via csv', 'data'=>$data],201);
         }
         return response()->json(['message'=>'You are not allowed to register via csv'],422);
 
