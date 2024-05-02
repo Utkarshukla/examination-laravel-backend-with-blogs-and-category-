e9e65cebed76b87f328b5260f95250c8c93ce6de
utkarshShukla <shukla@phantasm.co.in>
2024-05-02 10:20:28 +0530

    email template

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -7,7 +7,7 @@
     <p>Hello {{ $user->name }},</p>
     <p>Please click the following link to verify your email address:</p>
     @php
-        $url= env('FRONTEND_URL_R');
+        $url= config('services.frontend_url.frontend_url_r');
     @endphp
     <p><a href="{{$url}}/verify-token/?email={{ $user->email }}&token-id={{ $user->remember_token }}">Verify Now</a></p>
 </body>
